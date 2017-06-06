b003e25a4a016a8c5f64f25bfe8bb3a1168593c5
Joe <joe@Joes-MacBook-Pro.local>
2017-06-06 12:31:05 -0600

    added .config file for security

---
+++ .config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionString: 'postgres://mpawjhpkispfat:01e5847789c6ff4adf70c6eddbf3df26413ad0db96c370fb84aee136723a6e41@ec2-54-83-26-65.compute-1.amazonaws.com:5432/dblrdv1kj715un?ssl=true'
+}
